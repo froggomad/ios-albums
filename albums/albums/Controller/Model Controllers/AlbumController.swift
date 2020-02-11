3b46623a48e1b1c536e4bd24294705620d8c4b1d
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-02-10 20:08:09 -0800

    Create, Get, and Put functions working

--- albums/albums/Controller/Model Controllers/AlbumController.swift
+++ albums/albums/Controller/Model Controllers/AlbumController.swift
@@ -10,6 +10,100 @@ import Foundation
 
 class AlbumController {
     
+    //=======================
+    // MARK: - Properties
+    var albums: [Album] = []
+    let baseUrl = URL(string: "https://lambda-intermediate-codable.firebaseio.com/")!
+    typealias CompleteWithError = (Error?) -> ()
+    
+    //=======================
+    // MARK: - Create
+    func createAlbum(artist: String,
+                     coverArt: [URL],
+                     genres: [String],
+                     id: UUID,
+                     name: String,
+                     songs: [Album.Song]) {
+        
+        let album = Album(artist: artist,
+                      coverArt: coverArt,
+                      genres:genres,
+                      id:id,
+                      name:name,
+                      songs:songs)
+        albums.append(album)
+        putAlbum(album) { error in
+            if let error = error {
+                print(error)
+            }
+        }
+    }
+    
+    //=======================
+    // MARK: - Read/Get
+    func getAlbums(complete: @escaping CompleteWithError) {
+        var request = URLRequest(url: baseUrl.appendingPathExtension("json"))
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                complete(error)
+                return
+            }
+            if let data = data {
+                print(String(data: data, encoding: .utf8))
+                let decoder = JSONDecoder()
+                do {
+                    let albums = try decoder.decode([String:Album].self, from: data)
+                    self.albums = albums.map { $1 }
+                } catch {
+                    complete(error)
+                    return
+                }
+                print(self.albums)
+                complete(nil)
+            } else {
+                let error = NSError(domain: "AlbumController.getAlbums.decode", code: 999)
+                complete(error)
+            }
+        }.resume()
+    }
+    
+    //=======================
+    // MARK: - Update/PUT
+    func putAlbum(_ album: Album, complete: @escaping CompleteWithError) {
+        let url = baseUrl
+        .appendingPathComponent(album.id.uuidString)
+        .appendingPathExtension("json")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let encoder = JSONEncoder()
+        do {
+            request.httpBody = try encoder.encode(album)
+        } catch {
+            print(error)
+            complete(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let error = error {
+                complete(error)
+                return
+            }
+            if let response = response as? HTTPURLResponse,
+            response.statusCode != 200 {
+                print("error: \(response.statusCode)")
+                complete(error)
+            } else {
+                complete(nil)
+            }
+        }.resume()
+    }
+    
     class func testDecodingExampleAlbum() {
         let decoder = JSONDecoder()
         let urlPath = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json")
@@ -29,11 +123,12 @@ class AlbumController {
         do {
             let jsonData = try Data(contentsOf: urlPath!)
             album = try decoder.decode(Album.self, from: jsonData)
-            print(album)
+            print(String(describing: album))
         } catch {
             print(error)
         }
         let encoder = JSONEncoder()
-        print(String(data: try! encoder.encode(album), encoding: .utf8))
+        #warning("unstable! testing only!")
+        print(String(data: try! encoder.encode(album), encoding: .utf8)!)
     }
 }
--- albums/albums/Model/Album.swift
+++ albums/albums/Model/Album.swift
@@ -9,7 +9,8 @@
 import Foundation
 
 struct Album: Codable {
-    
+    //=======================
+    // MARK: - Properties
     let artist: String
     let coverArt: [URL]
     let genres: [String]
@@ -17,6 +18,8 @@ struct Album: Codable {
     let name: String
     let songs: [Song]
     
+    //=======================
+    // MARK: - CodingKeys
     enum AlbumKeys: String, CodingKey {
         case artist
         case coverArt
@@ -30,6 +33,8 @@ struct Album: Codable {
         case url
     }
     
+    //=======================
+    // MARK: - Decoder Init
     init(from decoder: Decoder) throws {
         do {
             let albumContainer = try decoder.container(keyedBy: AlbumKeys.self)
@@ -51,11 +56,33 @@ struct Album: Codable {
             throw error
         }
     }
+    //=======================
+    // MARK: - Standard Init
+    init(artist: String,
+         coverArt: [URL],
+         genres: [String],
+         id: UUID,
+         name: String,
+         songs: [Song]) {
+        self.artist = artist
+        self.coverArt = coverArt
+        self.genres = genres
+        self.id = id
+        self.name = name
+        self.songs = songs
+    }
     
+    //=======================
+    // MARK: - Encoding strategy
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: AlbumKeys.self)
+        var coverArtDict = [[String:URL]]()
+        for url in coverArt {
+            coverArtDict.append(["url":url])
+        }
+        
         do {
-            try container.encode(coverArt, forKey: .coverArt)
+            try container.encode(coverArtDict, forKey: .coverArt)
             try container.encode(artist, forKey: .artist)
             try container.encode(genres, forKey: .genres)
             try container.encode(id, forKey: .id)
@@ -67,10 +94,14 @@ struct Album: Codable {
     }
     
     struct Song: Codable {
+        //=======================
+        // MARK: - Properties
         let duration: String
         let id: UUID
         let title: String
         
+        //=======================
+        // MARK: - CodingKeys
         enum SongKeys: String, CodingKey {
             case id
             case title = "name"
@@ -81,10 +112,12 @@ struct Album: Codable {
             case title
         }
         
-        enum DurationKeys: String, CodingKey {
+        enum DurationKey: String, CodingKey {
             case duration
         }
         
+        //=======================
+        // MARK: - Decoder Init
         init(from decoder: Decoder) throws {
             let songContainer = try decoder.container(keyedBy: SongKeys.self)
             do {
@@ -92,19 +125,34 @@ struct Album: Codable {
                 title = try titleContainer.decode(String.self, forKey: .title)
                 id = try songContainer.decode(UUID.self, forKey: .id)
                 
-                let durationContainer = try songContainer.nestedContainer(keyedBy: DurationKeys.self, forKey: .duration)
+                let durationContainer = try songContainer.nestedContainer(keyedBy: DurationKey.self, forKey: .duration)
                 duration = try durationContainer.decode(String.self, forKey: .duration)
             } catch {
                 throw error
             }
         }
         
+        //=======================
+        // MARK: - Standard Init
+        init(duration: String,
+             id: UUID,
+             title: String) {
+            self.duration = duration
+            self.id = id
+            self.title = title
+        }
+        
+        //=======================
+        // MARK: - Encoding Strategy
         func encode(to encoder: Encoder) throws {
             var container = encoder.container(keyedBy: SongKeys.self)
+            let durationDict = [SongKeys.duration.rawValue:duration]
+            let titleDict = [TitleKey.title.rawValue:title]
+            
             do {
-                try container.encode(title, forKey: .title)
+                try container.encode(titleDict, forKey: .title)
                 try container.encode(id, forKey: .id)
-                try container.encode(duration, forKey: .duration)
+                try container.encode(durationDict, forKey: .duration)
             } catch {
                 throw error
             }
